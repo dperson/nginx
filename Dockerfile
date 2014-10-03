7936cde12de7ba9470663330a1b00aa4688c13cd
David Personette <dperson@gmail.com>
2014-10-03 13:53:23 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM ubuntu:14.04
+MAINTAINER David Personette <dperson@dperson.com>
+
+# Install nginx and uwsgi
+RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys \
+                00A6F0A3C300EE8C && \
+    echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu trusty main" >> \
+                /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get install -qqy --no-install-recommends openssl nginx && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* && \
+    ln -sf /dev/stdout /var/log/nginx/access.log && \
+    ln -sf /dev/stderr /var/log/nginx/error.log
+# Forward request and error logs to docker log collector
+
+# Configure
+COPY generate_ssl_key.sh /usr/local/bin/
+RUN mkdir -p /var/cache/nginx/cache && \
+    /usr/local/bin/generate_ssl_key.sh
+
+VOLUME ["/usr/share/nginx/html", "/etc/nginx"]
+
+EXPOSE 80 443
+
+CMD nginx -g "daemon off;"
---
+++ generate_ssl_key.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: generate_ssl_key.sh
+#
+#         USAGE: ./generate_ssl_key.sh
+#
+#   DESCRIPTION: Generate self-signed SSL keys
+#
+#       OPTIONS: FQDN, Country, State, Locality - for cert
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: David Personette (dperson@gmail.com),
+#  ORGANIZATION:
+#       CREATED: 09/28/2014 12:11
+#      REVISION: 1.0
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+#Required
+domain=${1:-home.lan}
+
+#Change to your details
+country=${2:-NO}
+state=${3:-Rogaland}
+locality=${4:-Sola}
+org=${5:-None}
+
+cert=/etc/ssl/certs/cert.pem
+key=/etc/ssl/private/key.pem
+
+openssl req -x509 -newkey rsa:2048 -keyout $key -out $cert -days 3600 \
+    -nodes -subj "/C=$country/ST=$state/L=$locality/O=$org${1:+/CN=$domain}"
