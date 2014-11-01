d6c317e76feae9ffe08e2ab4976219ac93651323
David Personette <dperson@gmail.com>
2014-11-01 18:07:23 -0400

    nginx.sh add proxy headers

--- nginx.sh
+++ nginx.sh
@@ -277,7 +277,9 @@ proxy() {
 \
     location '"$location"' {\
         proxy_pass       '"$service"';\
+        proxy_set_header Host $host;\
         proxy_set_header X-Real-IP $remote_addr;\
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\
 \
         proxy_buffering on;\
         proxy_buffers 8 4k;\
