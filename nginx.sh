193403a207fa90ad11f310f80dfc3a2cccf3728c
David Personette <dperson@gmail.com>
2016-02-02 01:15:48 -0500

    nginx.sh derp path for .well-known

--- nginx.sh
+++ nginx.sh
@@ -115,7 +115,7 @@ hsts() { local file=/etc/nginx/conf.d/hsts.conf \
 	EOF
     sed -i '/^ *listen 80/,/^}/ { /proxy_cache/,/^}/c\
 \
-    location ^~ /.well-known/ {\
+    location ^~ /\.well-known/ {\
         break;\
     }\
     rewrite ^(.*) https://$host$1 permanent;\
@@ -163,8 +163,8 @@ server {\
 '"$(grep -q 443 <<< $port && echo -e '\\\n    ssl on;\\
     ssl_certificate      /etc/nginx/ssl/cert.pem;\\
     ssl_certificate_key  /etc/nginx/ssl/key.pem;\\\n ')"'\
-    location ^~ /.well-known/ {\
-        root   /srv/www/.well-known/;\
+    location ^~ /\.well-known/ {\
+        root   /srv/www/.well-known;\
         break;\
     }\
     rewrite ^(.*) '"$destination"'$1 permanent;\
